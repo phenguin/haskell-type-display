57e3438849f5ec7007224ea1d1adf2f6aaee3dc7
Phenguin <jcullen@mit.edu>
2014-09-19 00:19:34 -0400

    Create minor mode

--- elisp/haskell-type-display.el
+++ elisp/haskell-type-display.el
@@ -1,3 +1,23 @@
 (require 'ghc-mod)
 (require 'shm)
+
+(defun test-fn ()
+  (message "First minor mode working.."))
+
+(defun hdt-inc ()
+  (interactive)
+  (setq hdt-test (+ 1 (or hdt-test 5))))
+
+(defun hdt-mode-setup ()
+  (set (make-local-variable 'hdt-test) 0))
+
+;;;###autoload
+(define-minor-mode hdt-mode
+  "Automatically display types of expressions surrounding point."
+  :lighter "HDT"
+  :keymap (let ((map (make-sparse-keymap)))
+	    (define-key map (kbd "C-c c-q a") 'test-fn)
+	    map)
+  (hdt-mode-setup))
+
 (provide 'haskell-type-display)
