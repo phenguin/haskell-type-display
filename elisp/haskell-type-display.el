dd1cfb2e48961a56c97e0f69e137bfee42e24183
Phenguin <jcullen@mit.edu>
2014-09-22 01:27:23 -0400

    Add timer and fix extra BOL whitespace

--- elisp/haskell-type-display.el
+++ elisp/haskell-type-display.el
@@ -3,12 +3,36 @@
 (require 'dash)
 (require 's)
 
-(defvar hdt/run-delay
+(defvar hdt/default-run-delay
   0.5
   "How much idle time must pass before updating the types display.")
 
+(defvar hdt/timer
+  nil
+  "The timer for the (maybe) currently running idle timer task.")
+
+(defun hdt/timer-enable (&optional secs)
+  (interactive)
+  (unless hdt/timer
+    (setq
+     hdt/timer
+     (run-with-idle-timer
+      (or secs hdt/default-run-delay)
+      t
+      'hdt-show-types-around-point))))
+
+(defun hdt/timer-disable ()
+  (interactive)
+  (if hdt/timer-running
+      (progn
+	(cancel-timer hdt/timer)
+	(setq hdt/timer nil))))
+
 (defun hdt-mode-setup ()
-  (set (make-local-variable 'hdt-test) 0))
+  (set (make-local-variable 'hdt/timer-running) t)
+  (set (make-local-variable 'hdt/timer) nil)
+  (set (make-local-variable 'hdt/default-run-delay) hdt/default-run-delay)
+  )
 
 ;;;###autoload
 (define-minor-mode hdt-mode
@@ -30,19 +54,29 @@
 	 (ec (- endcol 1))
 	 (spoint nil)
 	 (epoint nil))
-    (with-current-buffer buf
-      (goto-line sl)
-      (forward-char sc)
-      (setq spoint (point))
-      (goto-line el)
-      (forward-char ec)
-      (setq epoint (point))
-      (buffer-substring-no-properties spoint epoint))))
+    (save-excursion
+      (with-current-buffer buf
+	(goto-line sl)
+	(forward-char sc)
+	(setq spoint (point))
+	(goto-line el)
+	(forward-char ec)
+	(setq epoint (point))
+	(buffer-substring-no-properties spoint epoint)))))
+
 
 (defun hdt-to-type-annotation (tinfo &optional buffer)
   (let* ((posinfo (-slice tinfo 0 4))
-	 (type (nth 4 tinfo)))
-    `(,(apply 'hdt-buffer-substring-by-linecol (-concat posinfo (list buffer))) . ,type)))
+	 (startpos (-slice posinfo 0 2))
+	 (startcol (cadr startpos))
+	 (endpos (-slice posinfo 2 4))
+	 (type (nth 4 tinfo))
+	 (typestring (apply 'hdt-buffer-substring-by-linecol (-concat posinfo (list buffer))))
+	 (splitts (s-lines typestring))
+	 (cleaned (s-join "\n"
+			  (cons (car splitts)
+				(--map (substring it startcol) (cdr splitts))))))
+    (list cleaned type startpos endpos)))
 
 ;; Why didn't this already exist..?
 (defun ghc-run-async-command (cmd callback &optional numresults hook)
@@ -71,17 +105,22 @@
 (defun hdt-to-display-string (tinfo &optional buffer)
   (let* ((type-pair (hdt-to-type-annotation tinfo buffer))
 	 (exp (car type-pair))
-	 (type (cdr type-pair))
+	 (type (cadr type-pair))
 	 (sepchar (if (> (+ (length exp) (length type)) 80)
 		      "\n" " ")))
-    (concat (car type-pair) " ::" sepchar (cdr type-pair))))
+    (concat exp " ::" sepchar type)))
 
 (defun hdt-display-type-info (tinfos &optional outputbuffer)
   (interactive)
-  (let* ((buf (or outputbuffer "testbuf"))
-	 (tstrings (-map 'hdt-to-display-string tinfos)))
-    (with-current-buffer (get-buffer-create buf)
-      (setf (buffer-string)
-	    (s-join "\n\n" tstrings)))))
+  (if tinfos
+      (let* ((buf (or outputbuffer "testbuf"))
+	     (tstrings (-map 'hdt-to-display-string tinfos)))
+	(with-current-buffer (get-buffer-create buf)
+	  (setf (buffer-string)
+		(s-join "\n\n" tstrings))))))
+
+(defun hdt-show-types-around-point ()
+  (interactive)
+  (ghc-type-obtain-tinfos-async 'hdt-display-type-info))
 
 (provide 'haskell-type-display)
