d61faf418a54c2481a647654e9fca70948f38971
Phenguin <jcullen@mit.edu>
2014-09-23 02:08:33 -0400

    Change name to haskell-tsp and clean up

--- elisp/haskell-tsp.el
+++ elisp/haskell-tsp.el
@@ -1,43 +1,65 @@
+;;; haskell-tsp.el --- Haskell Types Surrounding Point Mode
+
+;; Copyright (c) 2014 Justin Cullen. All rights reserved.
+
+;; Author:    Justin Cullen <zenguine@gmail.com>
+;; Created:   23-Sep-2014
+;; Version:   0.1.0
+;; Keywords:  haskell, development
+;; Stability: unstable
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 (require 'ghc)
 (require 'eieio)
-(require 'shm)
 (require 'dash)
 (require 's)
 
-(defvar hdt/default-run-delay
+(defvar tsp/default-run-delay
   0.5
   "How much idle time must pass before updating the types display.")
 
-(defvar hdt/timer
+(defvar tsp/timer
   nil
   "The timer for the (maybe) currently running idle timer task.")
 
-(defvar hdt/current-einfos
+(defvar tsp/current-einfos
   nil
   "The current list of expressions around point")
 
-(defun hdt/timer-enable (&optional secs)
+(defun tsp/timer-enable (&optional secs)
   (interactive)
-  (unless hdt/timer
+  (unless tsp/timer
     (setq
-     hdt/timer
+     tsp/timer
      (run-with-idle-timer
-      (or secs hdt/default-run-delay)
+      (or secs tsp/default-run-delay)
       t
-      'hdt/show-types-around-point))))
+      'tsp/show-types-around-point))))
 
-(defun hdt/timer-disable ()
+(defun tsp/timer-disable ()
   (interactive)
-  (if hdt/timer
+  (if tsp/timer
       (progn
-	(cancel-timer hdt/timer)
-	(setq hdt/timer nil))))
+	(cancel-timer tsp/timer)
+	(setq tsp/timer nil))))
 
-(defun hdt/timer-toggle ()
+(defun tsp/timer-toggle ()
   (interactive)
-  (if hdt/timer
-      (hdt/timer-disable)
-    (hdt/timer-enable)))
+  (if tsp/timer
+      (tsp/timer-disable)
+    (tsp/timer-enable)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Setup class that holds information on haskell expressions
@@ -82,72 +104,75 @@
     (cons (line-number-at-pos) (current-column))))
 
 ;; Code modified slightly from shm-case-split
-(defun shm/case-split-parenthesized (name &optional expr-string)
-  "Prompt for a type then do a case split based on it."
-  (interactive (list (read-from-minibuffer "Type: ")))
-  (save-excursion
-    (let ((column (current-column))
-	  (case-expr (if expr-string
-			 expr-string
-		       "undefined")))
-      (insert "(")
-      (insert (concat "case " case-expr " "))
-      (if (not expr-string)
-	  (shm-evaporate (- (point) (+ 1 (length "undefined")))
-			 (- (point) 1)))
-      (insert "of\n")
-      (indent-to (+ column 2))
-      (shm-case-split-insert-alts
-       (shm-case-split-alts-from-data-decl
-        (haskell-process-get-data-type name)))
-      (insert ")"))))
+(eval-after-load 'shm
+  '(defun tsp:shm-case-split-parenthesized (name &optional expr-string)
+     "Prompt for a type then do a case split based on it."
+     (interactive (list (read-from-minibuffer "Type: ")))
+     (save-excursion
+       (let ((column (current-column))
+	     (case-expr (if expr-string
+			    expr-string
+			  "undefined")))
+	 (insert "(")
+	 (insert (concat "case " case-expr " "))
+	 (if (not expr-string)
+	     (shm-evaporate (- (point) (+ 1 (length "undefined")))
+			    (- (point) 1)))
+	 (insert "of\n")
+	 (indent-to (+ column 2))
+	 (shm-case-split-insert-alts
+	  (shm-case-split-alts-from-data-decl
+	   (haskell-process-get-data-type name)))
+	 (insert ")")))))
 
 (defmethod einfo-case-split ((x einfo))
-  (letf (((point-marker) (einfo-start x)))
-    (einfo-replace-string x "")
-    (shm/case-split-parenthesized
-     (shm-cleanup-type-string-for-case (einfo-type x))
-     (einfo-expr x))))
+  (if (require 'shm nil 'noerror)
+    (letf (((point-marker) (einfo-start x)))
+      (einfo-replace-string x "")
+      (tsp:shm-case-split-parenthesized
+       (shm-cleanup-type-string-for-case (einfo-type x))
+       (einfo-expr x)))
+    (error "Structured haskell mode is required to use case splitting.")))
 
 (defmethod einfo-replace-string ((x einfo) val)
   (with-current-buffer (einfo-buffer x)
     (setf (buffer-substring (- (einfo-start x) 1) (einfo-end x)) val)))
 
-(defun hdt/prompt-case-split (n)
+(defun tsp/prompt-case-split (n)
   (interactive (list (- (read-number "Case split which number expression?: " 1) 1)))
-  (if hdt/current-einfos
-      (einfo-case-split (nth n hdt/current-einfos))
-    (error "No expressions to choose from.  Call hdt/show-types-around-point or hdt/timer-enable first.")))
+  (if tsp/current-einfos
+      (einfo-case-split (nth n tsp/current-einfos))
+    (error "No expressions to choose from.  Call tsp/show-types-around-point or tsp/timer-enable first.")))
 
 ;;;###autoload
-(define-minor-mode hdt-mode
+(define-minor-mode tsp-mode
   "Automatically display types of expressions surrounding point."
-  :lighter "HDT"
+  :lighter "TSP"
   :keymap (let ((map (make-sparse-keymap)))
-	    (define-key map (kbd "C-c C-- t") 'hdt/show-types-around-point)
-	    (define-key map (kbd "C-c C-- z") 'hdt/show-display-buffer)
-	    (define-key map (kbd "C-c C-- T") 'hdt/timer-toggle)
-	    (define-key map (kbd "C-c C-- s") 'hdt/prompt-case-split)
+	    (define-key map (kbd "C-c C-- t") 'tsp/show-types-around-point)
+	    (define-key map (kbd "C-c C-- z") 'tsp/show-display-buffer)
+	    (define-key map (kbd "C-c C-- T") 'tsp/timer-toggle)
+	    (define-key map (kbd "C-c C-- s") 'tsp/prompt-case-split)
 	    map)
-  (setup-hdt-mode))
+  (setup-tsp-mode)
+  (hdt/timer-enable))
 
-(defun setup-hdt-mode ()
-  (set (make-local-variable 'hdt/timer-running) t)
-  (set (make-local-variable 'hdt/timer) nil)
-  (set (make-local-variable 'hdt/current-einfos) nil)
-  (set (make-local-variable 'hdt/default-run-delay) hdt/default-run-delay))
+(defun setup-tsp-mode ()
+  (set (make-local-variable 'tsp/timer-running) t)
+  (set (make-local-variable 'tsp/timer) nil)
+  (set (make-local-variable 'tsp/current-einfos) nil)
+  (set (make-local-variable 'tsp/default-run-delay) tsp/default-run-delay))
 
-(define-minor-mode hdt-display-buffer-mode
+(define-minor-mode tsp-display-buffer-mode
   "Mode for the buffer displaying the types."
-  :lighter "HDTDisplay"
+  :lighter "TSP-Display"
   :keymap (make-sparse-keymap)
-  (setup-hdt-display-buffer-mode))
+  (setup-tsp-display-buffer-mode))
 
-(defun setup-hdt-display-buffer-mode ()
+(defun setup-tsp-display-buffer-mode ()
   (turn-on-haskell-font-lock))
 
-
-(defun hdt:buffer-substring-by-linecol (startline startcol endline endcol &optional buffer)
+(defun tsp:buffer-substring-by-linecol (startline startcol endline endcol &optional buffer)
   (let* ((buf (or buffer (current-buffer)))
 	 ;; TODO: handle case of startcol==0
 	 (sl startline)
@@ -167,14 +192,14 @@
 	(buffer-substring-no-properties spoint epoint)))))
 
 
-(defun hdt:tinfo->einfo (tinfo &optional buffer)
+(defun tsp:tinfo->einfo (tinfo &optional buffer)
   (let* ((buf (or buffer (current-buffer)))
 	 (posinfo (-slice tinfo 0 4))
 	 (startpos (-slice posinfo 0 2))
 	 (startcol (cadr startpos))
 	 (endpos (-slice posinfo 2 4))
 	 (type (nth 4 tinfo))
-	 (typestring (apply 'hdt:buffer-substring-by-linecol (-concat posinfo (list buf))))
+	 (typestring (apply 'tsp:buffer-substring-by-linecol (-concat posinfo (list buf))))
 	 (splitts (s-lines typestring))
 	 (cleaned (s-join "\n"
 			  (cons (car splitts)
@@ -182,7 +207,7 @@
     (make-einfo-from-lc cleaned type (car startpos) (cadr startpos) (car endpos) (cadr endpos) buf)))
 
 ;; Why didn't this already exist..?
-(defun ghc-run-async-command (cmd callback &optional numresults hook)
+(defun tsp:ghc-run-async-command (cmd callback &optional numresults hook)
   (setq ghc-process-rendezvous nil)
   (setq ghc-process-results nil)
   (setq ghc-process-num-of-results (or numresults 1))
@@ -200,14 +225,14 @@
 	 (setq ghc-process-running nil))))
    nil hook))
 
-(defun ghc-type-obtain-tinfos-async (callback)
+(defun tsp:ghc-type-obtain-tinfos-async (callback)
   (let* ((ln (int-to-string (line-number-at-pos)))
 	(cn (int-to-string (1+ (current-column))))
 	(file (buffer-file-name))
 	(cmd (format "type %s %s %s\n" file ln cn)))
-    (ghc-run-async-command cmd callback nil 'ghc-type-fix-string)))
+    (tsp:ghc-run-async-command cmd callback nil 'ghc-type-fix-string)))
 
-(defun hdt:to-display-string (einfo &optional n buffer)
+(defun tsp:to-display-string (einfo &optional n buffer)
   (let* ((exp (einfo-expr einfo))
 	 (type (einfo-type einfo))
 	 (exp (if (string-match "\s+=\s+" exp)
@@ -216,39 +241,33 @@
 	 (s (concat exp " ::" sepchar type)))
     (if n (concat (number-to-string n) ": " s) s)))
 
-(defun hdt:get-display-buffer (&optional buffer)
-  (let* ((name (concat "*HDT:" (buffer-name (or buffer (current-buffer))) "*"))
+(defun tsp:get-display-buffer (&optional buffer)
+  (let* ((name (concat "*TSP:" (buffer-name (or buffer (current-buffer))) "*"))
 	 (buf (get-buffer name)))
     (if buf
 	buf
       (setq buf (get-buffer-create name))
       (with-current-buffer buf
-	(hdt-display-buffer-mode)
+	(tsp-display-buffer-mode)
 	buf))))
 
-(defun hdt/show-display-buffer ()
+(defun tsp/show-display-buffer ()
   (interactive)
-  (display-buffer (hdt:get-display-buffer)))
+  (display-buffer (tsp:get-display-buffer)))
 
-(defun hdt/display-type-info (tinfos &optional outputbuffer)
+(defun tsp/display-type-info (tinfos &optional outputbuffer)
   (interactive)
-  (let ((einfos (-map 'hdt:tinfo->einfo tinfos)))
-    (setq hdt/current-einfos einfos)
+  (let ((einfos (-map 'tsp:tinfo->einfo tinfos)))
+    (setq tsp/current-einfos einfos)
     (if tinfos
-	(let* ((buf (or outputbuffer (hdt:get-display-buffer)))
-	       (tstrings (-map-indexed (lambda (i x) (hdt:to-display-string x (+ i 1))) einfos)))
+	(let* ((buf (or outputbuffer (tsp:get-display-buffer)))
+	       (tstrings (-map-indexed (lambda (i x) (tsp:to-display-string x (+ i 1))) einfos)))
 	  (with-current-buffer (get-buffer-create buf)
 	    (setf (buffer-string)
 		  (s-join "\n\n" tstrings)))))))
 
-(defun hdt/show-types-around-point ()
+(defun tsp/show-types-around-point ()
   (interactive)
-  (ghc-type-obtain-tinfos-async 'hdt/display-type-info))
+  (tsp:ghc-type-obtain-tinfos-async 'tsp/display-type-info))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Test area ----------------------------
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-
-(provide 'haskell-type-display)
+(provide 'haskell-tsp)
